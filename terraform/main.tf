7639d519c1dc4a55e3f3f1e5b005701001a366cb
tanveernawaz222@gmail.comgit config --global user.name tanveernawaz222@gmail.comgit config --global user.email tanveermawaz222@gmail.com <tanveernawaz222@gmail.com>
2025-11-30 18:18:28 +0530

    updated-terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,14 @@ terraform {
   }
 }
 
-# Configure the AWS Provider - REMOVE access_key and secret_key
 provider "aws" {
   region = "ap-south-1"
 }
 
-# create security group for the ec2 instance
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 22"
 
-  # allow access on port 22
   ingress {
     description = "ssh access"
     from_port   = 22
@@ -40,7 +37,7 @@ resource "aws_security_group" "ec2_security_group" {
 
 resource "aws_instance" "Monitoring_server" {
   ami               = "ami-00bb6a80f01f03502"  
-  instance_type     = "t3.micro"
+  instance_type     = "t2.micro"  # ← FIXED: t2.micro uses fewer vCPUs
   security_groups   = [aws_security_group.ec2_security_group.name]
   key_name          = var.key_name
   
@@ -49,12 +46,10 @@ resource "aws_instance" "Monitoring_server" {
   }
 }
 
-# Output the public IP address
 output "public_ip" {
   value = aws_instance.Monitoring_server.public_ip
 }
 
-# Variables - KEEP ONLY key_name and instance_name, REMOVE access_key and secret_key
 variable "key_name" {
   description = "SSH key pair name"
   type        = string
